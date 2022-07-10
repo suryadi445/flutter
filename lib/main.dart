1ec1fabcb2779a5d78a307b5bc232916e9d60314
suryadi445 <suryadi.hhb@gmail.com>
2022-07-10 15:59:47 +0700

    flutter row dan column

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(title: Text('Flutter Demo')),
+            appBar: AppBar(title: Text('Flutter Row dan Column')),
             body: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
